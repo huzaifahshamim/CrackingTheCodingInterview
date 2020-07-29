d7d797bdb5e967f4fd043eddd2aa4c713df408e0
Huzaifah Shamim <mhs.huzaifah@gmail.com>
2020-07-29 12:33:39 -0500

    Added TCs for CheckPermutation

--- 1.2-CheckPermutation.py
+++ 1.2-CheckPermutation.py
@@ -6,18 +6,31 @@ other.
 
 def CheckPermutation(str1, str2):
     # we know that if the strings are of different length they can not be permutations of each other.
+    # Assumption: CASE SENSITIVE
     if len(str1) > len(str2) or len(str1) < len(str2):
         return False
     str1d = {}
     str2d = {}
     for char in range(len(str1)):
         if str1[char] in str1d:
-            str1d[char] += 1
+            str1d[str1[char]] += 1
         else:
-            str1d[char] += 1
+            str1d[str1[char]] = 1
         if str2[char] in str2d:
-            str2d[char] += 1
+            str2d[str2[char]] += 1
         else:
-            str2d[char] += 1
+            str2d[str2[char]] = 1
     return str1d == str2d
 
+
+ans1 = CheckPermutation('abcdefg', 'Gdcba')
+print(ans1)
+
+ans2 = CheckPermutation('cat', 'tac')
+print(ans2)
+
+ans3 = CheckPermutation('loppp', 'poplp')
+print(ans3)
+
+ans4 = CheckPermutation('DOG', 'ogd')
+print(ans4)
