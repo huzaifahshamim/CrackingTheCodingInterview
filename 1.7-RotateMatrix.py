33f15e7648fa3184c1baa4f93d935b076b536521
Huzaifah Shamim <mhs.huzaifah@gmail.com>
2020-07-29 17:11:34 -0500

    MainCode

--- 1.7-RotateMatrix.py
+++ 1.7-RotateMatrix.py
@@ -3,3 +3,17 @@ Given an image represented by an NxN matrix, where each pixel in the image is 4
 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
 """
 
+def RotateMatrix(matrix):
+    n = len(matrix)
+    for layer in range(n):
+        for cell in range(n):
+            tl = matrix[layer][cell + layer]
+            tr = matrix[cell + layer][n-1 - layer]
+            br = matrix[n-1 - layer][n-1 - cell - layer]
+            bl = matrix[n-1 - cell - layer][cell + layer]
+
+            matrix[layer][cell + layer] = bl
+            matrix[cell + layer][n - 1 - layer] = tl
+            matrix[n - 1 - layer][n - 1 - cell - layer] = tr
+            matrix[n - 1 - cell - layer][cell + layer] = br
+    return matrix
